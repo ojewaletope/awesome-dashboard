61f6f71817f907c12faf3db97449a4ccfd91c7a6
Tope Ojewale <topeojewale@gmail.com>
2018-05-04 16:46:09 +0100

    Made some adjustments to the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ var app = {
             $html.classList.add("nav-open");
             msc = 1;
           } else {
-            $html.classList.remove('nav-open');
+            $html.classList.remove("nav-open");
             bodyClick.remove();
             msc = 0;
           }
@@ -55,5 +55,52 @@ var app = {
         widthWatcher();
       }
     });
+  },
+
+  chartHandler: function() {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  },
+  introHandler: function() {
+    introJs().start();
   }
 };
